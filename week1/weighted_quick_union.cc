482687bedc8b65c616f6e5a0046850ae54cc680b
Kashyap Maduri <kashyapmaduri@outlook.com>
2015-10-05 00:12:31 -0700

    added the flattening optimization

--- week1/weighted_quick_union.cc
+++ week1/weighted_quick_union.cc
@@ -28,8 +28,10 @@ WeightedQuickUnion::WeightedQuickUnion(int n) {
 };
 
 int WeightedQuickUnion::root(int p) {
-    while(p != id[p])
+    while(p != id[p]) {
+        id[p] = id[id[p]];
         p = id[p];
+    }
     return p;
 }
 
